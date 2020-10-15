3f10d5902b04b4edaa9853b9eabc0858537c86c3
Olzumzum <evdokimova_olga_15@mail.ru>
2020-10-15 15:39:58 +0700

    finished screen

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("screen.fxml"));
         primaryStage.setTitle("Обработка изображения");
-        primaryStage.setScene(new Scene(root, 900, 300));
+        primaryStage.setScene(new Scene(root, 720, 350));
         primaryStage.show();
     }
 
