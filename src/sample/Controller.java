5ae6771076f3c5f045e4fa6cbd628cd7a9cb9275
Olzumzum <evdokimova_olga_15@mail.ru>
2020-10-15 16:05:02 +0700

    added click listeners

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,36 @@
 package sample;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.*;
+
 public class Controller {
+
+    @FXML
+    private Button loadFileButton;
+    @FXML
+    private Button filterOneButton;
+    @FXML
+    private Button filterTwoButton;
+
+
+    @FXML
+    public void onClickLoadFile(){
+        loadFileButton.setText("Thanks!");
+    }
+
+    @FXML
+    public void onClickApplyFilterOne(){
+        filterOneButton.setText("Apply1");
+    }
+
+    @FXML
+    public void onClickApplyFilterTwo(){
+        filterTwoButton.setText("Apply2");
+    }
+
+
 }
