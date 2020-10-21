0f35676ea54c101103dab055470403d534a2ab05
Olzumzum <evdokimova_olga_15@mail.ru>
2020-10-21 13:13:08 +0700

    working image conversion function

--- src/sample/workFiles/FileAdapter.java
+++ src/sample/workFiles/FileAdapter.java
@@ -11,47 +11,93 @@ public class FileAdapter {
     static BufferedImage image;
 
 
-    public void setImage(BufferedImage image){
+    public void setImage(BufferedImage image) {
         FileAdapter.image = image;
     }
 
     public File getFile(Color[][] data) {
 
-        BufferedImage bufferedImage = createImageFromPixels(image);
+//        BufferedImage bufferedImage = createImageFromPixels(image);
+        int[] pixels = createPixelArray(image);
+        BufferedImage bufferedImage = createImageFromPixels(pixels, image.getHeight(), image.getWidth());
 
         saveBufferedImage(bufferedImage);
 
         return null;
     }
 
-    /**
-     * создать изображение на основе полученного
-     * @param image
-     * @return
-     */
-    private BufferedImage createImageFromPixels(BufferedImage image){
-        BufferedImage bufferedImage = new BufferedImage(120,120, BufferedImage.TYPE_INT_RGB);
+
+    private BufferedImage createImageFromPixels(BufferedImage image) {
+        BufferedImage bufferedImage = new BufferedImage(120, 120, BufferedImage.TYPE_INT_RGB);
 
         int width = image.getWidth();
         int height = image.getHeight();
         int[] result = new int[height * width];
 
-        int count = 0;
-        for (int row = 0; row < height; row++) {
+
+        for (int row = 0, count = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                result[count] = image.getRGB(row, col);
-                bufferedImage.setRGB(row, col, result[count]);
+                result[count++] = image.getRGB(row, col);
             }
-            count++;
+
+        }
+
+
+        for (int row = 0, count = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                bufferedImage.setRGB(row, col, result[count++]);
+            }
+
         }
 
         return bufferedImage;
     }
 
     /**
+     * создать массив пикселей на основе
+     * полученного изображения
+     *
+     * @param image входное изображение
+     * @return массив пикселей, соответствующий входному
+     * изобрашению
+     */
+    private int[] createPixelArray(BufferedImage image) {
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int[] pixels = new int[height * width];
+
+        for (int row = 0, count = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                pixels[count++] = image.getRGB(row, col);
+            }
+
+        }
+        return pixels;
+    }
+
+    /**
+     * Преобразовать массив пикселей в изображение
+     * @param pixles - входной массив пикселей
+     * @param height - высота итогового изображения
+     * @param width = ширина итогового изображения
+     * @return
+     */
+    private BufferedImage createImageFromPixels(int[] pixles, int height, int width) {
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int row = 0, count = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                bufferedImage.setRGB(row, col, pixles[count++]);
+            }
+
+        }
+        return bufferedImage;
+    }
+
+    /**
      * удалить временный файл, созданный отфильтрованным объектом
      */
-    public void deleteTemporaryFile(){
+    public void deleteTemporaryFile() {
         String savePath = System.getProperty("user.dir") +
                 System.getProperty("file.separator") + "src" + System.getProperty("file.separator") +
                 "res" + System.getProperty("file.separator") + "txt.jpeg";
@@ -65,10 +111,10 @@ public class FileAdapter {
         }
     }
 
-    private File saveBufferedImage(BufferedImage bi){
+    private File saveBufferedImage(BufferedImage bi) {
         String savePath = System.getProperty("user.dir") +
                 System.getProperty("file.separator") + "src" + System.getProperty("file.separator") +
-                "res" + System.getProperty("file.separator") + "temporary.jpg" ;
+                "res" + System.getProperty("file.separator") + "temporary.jpg";
 
         File file = new File(savePath);
         try {
@@ -80,4 +126,4 @@ public class FileAdapter {
     }
 
 
-    }
+}
