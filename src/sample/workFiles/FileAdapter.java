ae0b41bc5784ef1f8138b1a0b166cfbaae9abb83
Olzumzum <evdokimova_olga_15@mail.ru>
2020-10-21 13:41:39 +0700

    show image@

--- src/sample/workFiles/FileAdapter.java
+++ src/sample/workFiles/FileAdapter.java
@@ -3,10 +3,14 @@ package sample.workFiles;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class FileAdapter {
+
+    String savePath = System.getProperty("user.dir") +
+            System.getProperty("file.separator") + "src" + System.getProperty("file.separator") +
+            "res" + System.getProperty("file.separator") + "temporary.jpg";
+
 
     static BufferedImage image;
 
@@ -16,42 +20,17 @@ public class FileAdapter {
     }
 
     public File getFile(Color[][] data) {
-
-//        BufferedImage bufferedImage = createImageFromPixels(image);
         int[] pixels = createPixelArray(image);
         BufferedImage bufferedImage = createImageFromPixels(pixels, image.getHeight(), image.getWidth());
 
         saveBufferedImage(bufferedImage);
 
-        return null;
+        File file = new File(savePath);
+
+
+        return file;
     }
 
-
-    private BufferedImage createImageFromPixels(BufferedImage image) {
-        BufferedImage bufferedImage = new BufferedImage(120, 120, BufferedImage.TYPE_INT_RGB);
-
-        int width = image.getWidth();
-        int height = image.getHeight();
-        int[] result = new int[height * width];
-
-
-        for (int row = 0, count = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                result[count++] = image.getRGB(row, col);
-            }
-
-        }
-
-
-        for (int row = 0, count = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                bufferedImage.setRGB(row, col, result[count++]);
-            }
-
-        }
-
-        return bufferedImage;
-    }
 
     /**
      * создать массив пикселей на основе
@@ -78,21 +57,22 @@ public class FileAdapter {
 
     /**
      * Преобразовать массив пикселей в изображение
-     * @param pixles - входной массив пикселей
+     * @param pixels - входной массив пикселей
      * @param height - высота итогового изображения
      * @param width = ширина итогового изображения
      * @return
      */
-    private BufferedImage createImageFromPixels(int[] pixles, int height, int width) {
+    private BufferedImage createImageFromPixels(int[] pixels, int height, int width) {
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         for (int row = 0, count = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                bufferedImage.setRGB(row, col, pixles[count++]);
+                bufferedImage.setRGB(row, col, pixels[count++]);
             }
 
         }
         return bufferedImage;
     }
+
 
     /**
      * удалить временный файл, созданный отфильтрованным объектом
@@ -112,9 +92,6 @@ public class FileAdapter {
     }
 
     private File saveBufferedImage(BufferedImage bi) {
-        String savePath = System.getProperty("user.dir") +
-                System.getProperty("file.separator") + "src" + System.getProperty("file.separator") +
-                "res" + System.getProperty("file.separator") + "temporary.jpg";
 
         File file = new File(savePath);
         try {
